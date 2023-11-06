9b37efd72fddd18cca0c92420d6b2519aab74dc2
Leandra <132718156+Leandra-arias@users.noreply.github.com>
2023-11-06 18:30:31 -0300

    Create r-file.r

---
+++ r-file.r
@@ -0,0 +1,35 @@
+
+R version 4.3.1 (2023-06-16 ucrt) -- "Beagle Scouts"
+Copyright (C) 2023 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+R es un software libre y viene sin GARANTIA ALGUNA.
+Usted puede redistribuirlo bajo ciertas circunstancias.
+Escriba 'license()' o 'licence()' para detalles de distribucion.
+
+R es un proyecto colaborativo con muchos contribuyentes.
+Escriba 'contributors()' para obtener más información y
+'citation()' para saber cómo citar R o paquetes de R en publicaciones.
+
+Escriba 'demo()' para demostraciones, 'help()' para el sistema on-line de ayuda,
+o 'help.start()' para abrir el sistema de ayuda HTML con su navegador.
+Escriba 'q()' para salir de R.
+
+[Previously saved workspace restored]
+
+ ### "Quantium Virtual Internship - Retail Strategy and Analytics - Task 1"
+ 
+ ### install packages and libraries
+ 
+install.packages("data.table")
+Installing package into ‘C:/Users/USUARIO/AppData/Local/R/win-library/4.3’
+(as ‘lib’ is unspecified)
+
+package ‘data.table’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+        C:\Users\USUARIO\AppData\Local\Temp\RtmpKsb7o6\downloaded_packages
+library(ggplot2)
+library(data.table)
+### download dataset transaction format csv
+
